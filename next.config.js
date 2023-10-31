f0c41069eac9badbb3dfdd21a13af8155718075e
Xochitl Mora <124631246+featherstoned@users.noreply.github.com>
2023-10-31 13:02:39 -1000

    first commit - working app - v3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
+//& code from tutorial
+// // /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         port: "1337",
+//         pathname: "/uploads/**",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+//& this fixed it, from bard
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/checkout.js
@@ -0,0 +1,26 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useInitialRender } from "@/utils/useInitialRender";
+import CheckoutForm from "@/components/CheckoutForm";
+import CheckoutCart from "@/components/CheckoutCart";
+const stripePromise = loadStripe("pk_test_51Ny2fbC4boavpkWBuFIgu2z4VzlWTKTcdT2UrVrQO8GyGgizVpEGlOxFQLqbqioZK4NUjSsCJLYWiVIs6LxQtwdh00P23j4y5X");
+
+export default function Checkout() {
+  const initialRender = useInitialRender();
+  if (!initialRender) return null;
+
+  return (
+    <section className="container mx-auto py-24">
+      <div className="grid grid-cols-5 gap-4">
+        <div className="col-span-2">
+          <CheckoutCart />
+        </div>
+        <div className="col-span-3">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </div>
+    </section>
+  );
+}
