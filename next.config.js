3c02a2407418fb9e50fb53c4f666f960d9585256
Xochitl Mora <124631246+featherstoned@users.noreply.github.com>
2023-11-02 10:00:14 -1000

    updates to deploy images and update url

--- next.config.js
+++ next.config.js
@@ -16,8 +16,32 @@
 
 // module.exports = nextConfig;
 
-//& this fixed it, from bard
+//& this fixed it, from bard, running locally
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         port: "1337",
+//         pathname: "/uploads/**",
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "127.0.0.1",
+//         port: "1337",
+//         pathname: "/uploads/**",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+//& this for deployment
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -33,6 +57,11 @@ const nextConfig = {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "strapi-g6mo.onrender.com",
         pathname: "/uploads/**",
       },
     ],
